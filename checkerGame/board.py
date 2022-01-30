5c98455be5e68797940be4f59b4398b8c51d5a02
Eric Eaton <92324662+eric-eaton@users.noreply.github.com>
2022-01-30 10:57:29 -0500

    Update board.py

--- checkerGame/board.py
+++ checkerGame/board.py
@@ -3,14 +3,27 @@ A Checkers board is an 8x8 array. This will be represented by a numpy array. In 
 or unoccupied. This will be represented with NaN values.
 """
 import numpy as np
+from tile import Tile
 
-class checkerBoard:
+
+class CheckerBoard:
+
+
     """
     CONSTRUCTOR
     This constructor creates an empty 8x8 numpy array and then fills it with NaN values.
     """
     def __init__(self):
         self.board = np.empty((8, 8),dtype='object')
+        self.fill_board()
+
+    def fill_board(self):
+        for column in range (0,8):
+            for row in range(0,8):
+                tile = Tile()
+                tile.set_position(column,row+1)
+                tile.set_valid()
+                self.board[row,column] = tile
     """
     GETTER
     This function returns the board created by the constructor
