293b5a898a98af3b1db1d382303980b69b556c3d
Eric Eaton <92324662+eric-eaton@users.noreply.github.com>
2022-02-02 10:13:24 -0500

    Update createGame.py

--- checkerGame/createGame.py
+++ checkerGame/createGame.py
@@ -2,42 +2,33 @@
 Creates a game of checkers
 This uses the board.py, pieces.py, and player.py to create the game
 """
-from board import checkerBoard
-from piece import checkerPieces
+from board import CheckerBoard
 
-class checkerGame:
+class CheckerGame:
     def __init__(self):
-        self.gameBoard = checkerBoard()
-        self.gameBoard = self.gameBoard.getBoard()
-        self.pieces = checkerPieces()
-        self.red, self.black = self.pieces.getPieceList()
-
-
+        self.gameBoard = CheckerBoard()
+        self.fillBoard()
 
     def fillBoard(self):
-        count = 0
-        for row in range(0,3):
-            if row == 0 or row == 2:
-                for column in range(0,7,2):
-                    self.gameBoard[row,column] = self.black[count]
+        #place black pieces first
+        count = 1
+        for row in range (0,4):
+            for column in range(0,8):
+                if self.gameBoard[row,column].get_valid():
+                    self.gameBoard[row,column].occupy('black', count)
                     count+=1
-            else:
-                for column in range(1,8,2):
-                    self.gameBoard[row,column] = self.black[count]
-                    count+=1
-        count = 0
-        for row in range(5,8):
-            if row == 6:
-                for column in range(0,7,2):
-                    self.gameBoard[row,column] = self.red[count]
-                    count+=1
-            else:
-                for column in range(1,8,2):
-                    self.gameBoard[row,column] = self.red[count]
+
+        #place red pieces
+        count = 1
+        for row in range (5,8):
+            for column in range(0,8):
+                if self.gameBoard[row,column].get_valid():
+                    self.gameBoard[row,column].occupy('red', count)
                     count+=1
 
 
 
-"""from createGame import checkerGame
-game = checkerGame()
-game.fillBoard()"""
+"""
+from createGame import CheckerGame
+game = CheckerGame()
+"""
