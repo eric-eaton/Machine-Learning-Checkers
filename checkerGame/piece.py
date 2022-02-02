d020513dee921096e9e252fa9e5ba13d4d67c60a
Eric Eaton <92324662+eric-eaton@users.noreply.github.com>
2022-02-02 10:13:54 -0500

    Update piece.py

--- checkerGame/piece.py
+++ checkerGame/piece.py
@@ -3,31 +3,21 @@ A checkers game has 24 pieces, 12 black and 12 red.
 """
 
 
-class checkerPieces:
+class CheckerPiece:
     """
     CONSTRUCTOR
-    This constructor intializes the red pieces with the character X and the black pieces with the character Y
-    It also initializes two lists which will contain the pieces. It then calls fillPieceList() to fill the lists
+    This constructor initializes a checkerPiece object. The constructor takes three arguments, the color, location, and
+    label of the piece.
     """
-    def __init__ (self):
-        self.redPiece = 'red'
-        self.blackPiece = 'black'
-        self.redList = []
-        self.blackList = []
-        self.fillPieceList()
+    def __init__ (self, color, location, label):
+        self.color = color
+        self.location = location
+        self.captured = False
+        self.king = False
+        self.label = label
 
     """
-    HELPER
-    This function executes 2 for loops which fill each list
+    PLACEHOLDER
     """
-    def fillPieceList(self):
-        for i in range(0,12):
-            self.redList.append(self.redPiece)
-
-        for i in range(0,12):
-            self.blackList.append(self.blackPiece)
-
-
-
-    def getPieceList(self):
-        return self.redList, self.blackList
+    def move(self,newLocation):
+        return newLocation
